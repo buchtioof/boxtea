e4302aa54d00f9b2d9dcdc1c2a8036d1f3e49b33
ramzouillee <48603083+buchtioof@users.noreply.github.com>
2026-04-24 16:29:37 +0200

    v0.1.5 updating uninstaller

--- uninstaller.sh
+++ uninstaller.sh
@@ -7,13 +7,10 @@ SUCCESS='\033[0;32m'    # GREEN
 WARNING='\033[0;33m'    # YELLOW
 ECM='\033[0m'           # END COLOR MESSAGE
 
-read -r -p "You are about to uninstall completly Boxtea daemon, your configurations and data would be deleted. Continue? [y/N] " response < /dev/tty
-
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    echo -e "\n${WARNING}> 1/3 Stoping Boxtea daemon and processus...${ECM}"
-    sudo systemctl stop boxtea > /dev/null 2>&1 || true
-    sudo systemctl disable boxtea > /dev/null 2>&1 || true
-    sudo rm -f /etc/systemd/system/boxtea.service
+echo -e "\n${WARNING}> 1/3 Stoping Boxtea daemon and processus...${ECM}"
+sudo systemctl stop boxtea > /dev/null 2>&1 || true
+sudo systemctl disable boxtea > /dev/null 2>&1 || true
+sudo rm -f /etc/systemd/system/boxtea.service
     sudo systemctl daemon-reload
 
     echo -e "${WARNING}> 2/3 Deleting Boxtea data...${ECM}"
@@ -26,7 +23,3 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo -e "${SUCCESS}========================================================================${ECM}"
     echo -e "${SUCCESS}  [SUCCESS] Boxtea has been completely uninstalled!${ECM}"
     echo -e "${SUCCESS}========================================================================${ECM}"
-else
-    echo -e "\n${SUCCESS}> Cancelled process.${ECM}"
-    exit 0
-fi
