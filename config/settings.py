91685b9deef1486ac6ae08adc1c43c74ed841124
ramzouillee <48603083+buchtioof@users.noreply.github.com>
2026-04-24 23:18:22 +0200

    v0.1.10 fix css bug

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
